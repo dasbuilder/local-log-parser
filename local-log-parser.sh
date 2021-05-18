798a5dc75efcd988d8eb6829771ad3293f4807b2
Spencer Anderson <se.anderson88@gmail.com>
2021-05-18 18:11:05 -0500

    Added more newlines in setup.sh output. Added another comment about sed in local-log-parser.sh

--- local-log-parser.sh
+++ local-log-parser.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-# Takes the file name as the only argument. Uses zcat for compressed log files
+# Takes the file name as the only argument.
+# Uses sed to remove color and some formatting to cleanup the output.
+
 jq -r '.timestamp, .message' "$1" | 
 sed 's|\\n|\n|g; s|\\t|\t|g; s|^\"||g; s|\"$||g; s|\\u001b\[0m||g; s|\\u001b\[1\;31m\\r||g; s|\\u001b\[1\;\?32m||g; s|\\u001b\[36m||g; s|\\u001b\[32m||g'
--- setup.sh
+++ setup.sh
@@ -38,29 +38,34 @@ brewpath="$(which brew)";
 # Detecting if jq is installed, if it's not we need to install it either using brew on Mac. 
 if [[ ! -f "$jqpath" ]]; then
 	printf "\njq isn't installed. Let's install it...\n" 
-	
+	sleep 2;
 	if [[ $macos ]]; then
-		printf "MacOSX or MacOS detected...let's see if brew is available\n"
+		printf "\nMacOSX or MacOS detected...let's see if brew is available\n"
+		sleep 1;
 		# Checking if brew is installed or not
 		if [[ ! -f "$brewpath" ]]; then
-			printf "You don't have brew installed. If you want to install it, enter y for Yes and n for No.\n";
+			printf "\nYou don't have brew installed. If you want to install it, enter y for Yes and n for No.\n";
 			read -r answer;
+			sleep 2;
 				if [[ $answer == [Yy] ]]; then
 					printf "Installing brew...\n"; 
 					/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && 
 					printf "Installing jq now with 'brew install jq'\n" && 
 					/usr/local/bin/brew install jq;
 				else
-					printf "You opted to skip installing brew.\nInstalling jq manually...\n"
+					printf "\nYou opted to skip installing brew.\nInstalling jq manually...\n"
 				fi
 				# Installing jq manually
+				sleep 2;
 				printf "Downloading jq executable...\n";
 				curl -sLO --output-dir ~/Downloads/ https://github.com/stedolan/jq/releases/download/jq-1.6/jq-osx-amd64 && 
 				printf "Installing\n" && 
 				/bin/bash ~/Downloads/jq-osx-amd64
-		else
-			printf "Installing jq now with 'brew install jq'\n" && 
+		else 
+			# Installing jq using brew, since we already have brew 
+			printf "\nInstalling jq now with 'brew install jq'\n" && 
 			/usr/local/bin/brew install jq;
+			sleep 2;
 		fi	# End of brew check if block
 	fi # End of OS check if block
 else # Else for jqpath if block
@@ -68,23 +73,26 @@ else # Else for jqpath if block
 fi
 
 # With jq installed, we need to download the application files
+echo;
 printf "Downloading application files...\n"
 curl -sL -O --output-dir ~/Downloads/ "https://raw.githubusercontent.com/dasbuilder/local-log-parser/master/local-log-parser.sh"; 
 sleep 2;
 
-printf "Installing the program...\n";
+printf "\nInstalling the program...\nYou might be asked for your administrator or root password. Enter it when prompted.\n";
 chmod +x ~/Downloads/local-log-parser.sh && sudo mv -v ~/Downloads/local-log-parser.sh /usr/local/bin/local-log-parser.sh
+sleep 2;
 
 if [[ -z "$profilefile" ]]; then
-	printf "Could not automatically determine which profile you are using. Please manually set an alias in your profile file. Your shell type is: %s\n" "$SHELL"
+	printf "\nCould not automatically determine which profile you are using. Please manually set an alias in your profile file. Your shell type is: %s\n" "$SHELL"
 else
-	echo "lp='/usr/local/bin/local-log-parser.sh'" >> "$profilefile";
+	echo "alias lp='/usr/local/bin/local-log-parser.sh'" >> "$profilefile";
 	printf "\nAn alias has been created for you called %s in your %s\n" '"lp"' "$profilefile";
 	printf "If you would like to change this, edit your %s manually.\n" "$profilefile"
+	sleep 2;
   printf "Please run %s to reload your profile to use your new alias.\n" "source $profilefile"
 fi; 
 
-printf "\nTo use the program, run %s or use your preferred alias.\nE.g. 'lp local-lightning.log'\n" "'./usr/local/bin/local-log-parser.sh local-lightning.log'"
+printf "\nTo use the program, run %s or use your preferred alias.\nE.g. 'lp local-lightning.log'\n" "'$SHELL /usr/local/bin/local-log-parser.sh local-lightning.log'"
 printf "\nIf you experience issues, please fill out an issue on my github page here: %s\n\nThanks and enjoy!" "https://github.com/dasbuilder/local-log-parser/issues"
 
 
